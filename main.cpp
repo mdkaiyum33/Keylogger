d4b6c7d2da0c401d966e421204ed2be05b3298a4
mdkaiyum33 <mdkaiyumhossain75@proton.me>
2025-10-21 11:57:19 +0600

    Write Log To File

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void log(const string &text) {
+    ofstream data("data.txt", ios::app);
+    if (data.is_open()) {
+        data << text;
+    } 
+    else {
+        cout << "Failed to open file !";
+    }
+}
+
 int main(){
-    cout << "Hello Keylogger!";    
+    log("Starting the log...\n");
+    return 0;
 }
